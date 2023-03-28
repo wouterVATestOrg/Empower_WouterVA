ce94eb3d39f5b2a8fdcd4d8d11a8a31f0cdfeece
wvanaeken <wouter.van.aeken@telenetgroup.be>
2023-03-28 14:15:05 +0200

    Add project files.

---
+++ Empower_WouterVA_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_WouterVA_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_WouterVA_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("1173131B-0CF9-427E-A536-21DBF7564F2F")]
+[assembly: AssemblyVersion("1.0.0.0")]
