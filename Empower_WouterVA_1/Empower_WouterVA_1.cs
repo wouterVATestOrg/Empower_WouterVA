0a77b0252c1f8a1e80b73146956f3356840220d6
wvanaeken <wouter.van.aeken@telenetgroup.be>
2023-03-28 16:15:56 +0200

    list elements

--- Empower_WouterVA_1/Empower_WouterVA_1.cs
+++ Empower_WouterVA_1/Empower_WouterVA_1.cs
@@ -56,7 +56,8 @@ namespace Empower_WouterVA_1
 	using System.Globalization;
 	using System.Text;
 	using Skyline.DataMiner.Automation;
-	
+	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -68,7 +69,13 @@ namespace Empower_WouterVA_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello world!");
+			var myDms = engine.GetDms();
+			var elements = myDms.GetElements();
+			foreach(var element in elements)
+			{
+				engine.GenerateInformation(element.Name);
+			}
+
 		}
 	}
 }
